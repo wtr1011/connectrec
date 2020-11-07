9e5b510254ba2c7b28e85818fad1d1d227bf0127
Wataru Hamanishi <>
2020-11-07 18:52:11 +0900

    testcode

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,22 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+window.fn = {};
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+window.fn.toggleMenu = function () {
+    document.getElementById('appSplitter').right.toggle();
+};
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+window.fn.loadView = function (index) {
+    document.getElementById('appTabbar').setActiveTab(index);
+    document.getElementById('sidemenu').close();
+};
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
+window.fn.loadLink = function (url) {
+    window.open(url, '_blank');
+};
+
+window.fn.pushPage = function (page, anim) {
+    if (anim) {
+        document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title }, animation: anim });
+    } else {
+        document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title } });
+    }
+};
