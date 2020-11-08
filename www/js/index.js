657d538dbd74d1474c2a8bc14a4e957515b27bcf
Wataru Hamanishi <>
2020-11-08 16:45:46 +0900

    ログイン画面の作成

--- www/js/index.js
+++ www/js/index.js
@@ -1,22 +1,10 @@
-window.fn = {};
+var login = function () {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
 
-window.fn.toggleMenu = function () {
-    document.getElementById('appSplitter').right.toggle();
-};
-
-window.fn.loadView = function (index) {
-    document.getElementById('appTabbar').setActiveTab(index);
-    document.getElementById('sidemenu').close();
-};
-
-window.fn.loadLink = function (url) {
-    window.open(url, '_blank');
-};
-
-window.fn.pushPage = function (page, anim) {
-    if (anim) {
-        document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title }, animation: anim });
+    if (username === 'bob' && password === 'secret') {
+        ons.notification.alert('Congratulations!');
     } else {
-        document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title } });
+        ons.notification.alert('Incorrect username or password.');
     }
 };
